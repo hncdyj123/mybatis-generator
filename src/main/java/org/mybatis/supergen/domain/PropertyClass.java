ceefddf771d58b9096f0ec82d6b06f038409581d
hncdyj123 <hncdyj123@163.com>
2019-06-11 14:14:55 +0800

    优化代码

--- src/main/java/org/mybatis/supergen/domain/PropertyClass.java
+++ src/main/java/org/mybatis/supergen/domain/PropertyClass.java
@@ -3,6 +3,8 @@ package org.mybatis.supergen.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.Data;
+
 /**
  * 
  * @ClassName: PropertyClass
@@ -11,6 +13,7 @@ import java.util.List;
  * @date 2015年11月21日 下午12:24:38
  *
  */
+@Data
 public class PropertyClass {
 	// 类名
 	private String className;
@@ -27,76 +30,28 @@ public class PropertyClass {
 	// 主键List
 	private List<String> primaryKeyList = new ArrayList<String>();
 
-	public String getClassName() {
-		// 处理多主键问题
-		if (this.getPrimaryKeyList().size() > 1) {
-			return className + "Key";
-		}
-		return className;
-	}
+	// public String getClassName() {
+	// // 处理多主键问题
+	// if (this.getPrimaryKeyList().size() > 1) {
+	// return className + "Key";
+	// }
+	// return className;
+	// }
 
-	public void setClassName(String className) {
-		this.className = className;
-	}
-
-	public String getTableName() {
-		return tableName;
-	}
-
-	public void setTableName(String tableName) {
-		this.tableName = tableName;
-	}
-
-	public String getPackageName() {
-		return packageName;
-	}
-
-	public void setPackageName(String packageName) {
-		this.packageName = packageName;
-	}
-
-	public String getPriJava() {
-		// 多主键只拿第一个主键
-		for (String primaryKey : primaryKeyList) {
-			for (Column column : columns) {
-				if (column.getDatabaseName().equalsIgnoreCase(primaryKey)) {
-					column.setColumnKey("PRI");
-					priJava = column.getFieldName();
-					priJavaType = column.getJdkType();
-					break;
-				}
-			}
-			break;
-		}
-		return priJava;
-	}
-
-	public void setPriJava(String priJava) {
-		this.priJava = priJava;
-	}
-
-	public List<Column> getColumns() {
-		return columns;
-	}
-
-	public void setColumns(List<Column> columns) {
-		this.columns = columns;
-	}
-
-	public List<String> getPrimaryKeyList() {
-		return primaryKeyList;
-	}
-
-	public void setPrimaryKeyList(List<String> primaryKeyList) {
-		this.primaryKeyList = primaryKeyList;
-	}
-
-	public String getPriJavaType() {
-		return priJavaType;
-	}
-
-	public void setPriJavaType(String priJavaType) {
-		this.priJavaType = priJavaType;
-	}
+	// public String getPriJava() {
+	// // 多主键只拿第一个主键
+	// for (String primaryKey : primaryKeyList) {
+	// for (Column column : columns) {
+	// if (column.getDatabaseName().equalsIgnoreCase(primaryKey)) {
+	// column.setColumnKey("PRI");
+	// priJava = column.getFieldName();
+	// priJavaType = column.getJdkType();
+	// break;
+	// }
+	// }
+	// break;
+	// }
+	// return priJava;
+	// }
 
 }
--- src/main/java/org/mybatis/supergen/domain/TemplateInfoDesc.java
+++ src/main/java/org/mybatis/supergen/domain/TemplateInfoDesc.java
@@ -1,5 +1,9 @@
 package org.mybatis.supergen.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * 模板描述信息
  * 
@@ -7,6 +11,9 @@ package org.mybatis.supergen.domain;
  * @date 2015年11月26日 下午2:41:28
  *
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TemplateInfoDesc {
 	/** 模板文件位置 **/
 	private String templatePath;
@@ -21,65 +28,11 @@ public class TemplateInfoDesc {
 	/** 文件后缀 **/
 	private String fileSuffixes;
 
-	public TemplateInfoDesc() {
-
-	}
-
 	public TemplateInfoDesc(String templatePath, String templateName, String outFilePath, String filePrefix, String fileSuffixes) {
-		super();
 		this.templatePath = templatePath;
 		this.templateName = templateName;
 		this.outFilePath = outFilePath;
 		this.filePrefix = filePrefix;
 		this.fileSuffixes = fileSuffixes;
 	}
-
-	public String getTemplatePath() {
-		return templatePath;
-	}
-
-	public void setTemplatePath(String templatePath) {
-		this.templatePath = templatePath;
-	}
-
-	public String getTemplateName() {
-		return templateName;
-	}
-
-	public void setTemplateName(String templateName) {
-		this.templateName = templateName;
-	}
-
-	public String getOutFilePath() {
-		return outFilePath;
-	}
-
-	public void setOutFilePath(String outFilePath) {
-		this.outFilePath = outFilePath;
-	}
-
-	public PropertyClass getPropertyClass() {
-		return propertyClass;
-	}
-
-	public void setPropertyClass(PropertyClass propertyClass) {
-		this.propertyClass = propertyClass;
-	}
-
-	public String getFilePrefix() {
-		return filePrefix;
-	}
-
-	public void setFilePrefix(String filePrefix) {
-		this.filePrefix = filePrefix;
-	}
-
-	public String getFileSuffixes() {
-		return fileSuffixes;
-	}
-
-	public void setFileSuffixes(String fileSuffixes) {
-		this.fileSuffixes = fileSuffixes;
-	}
-
 }
